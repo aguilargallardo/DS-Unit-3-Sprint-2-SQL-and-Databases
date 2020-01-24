3b8d32e8846fc6b545779f310849bedd936e8f59
aguilargallardo <aguilargallardo@users.noreply.github.com>
2020-01-24 08:26:32 -0800

    Sprint Challenge

---
+++ Sprint Challenge/demo_data.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+conn = sqlite3.connect('demo_data.sqlite3')
+
+#cursor
+cur = conn.cursor()
+
+#dropping  existing table
+cur.execute("DROP TABLE IF EXISTS demo")
+
+#making table
+cur.execute("""
+CREATE TABLE demo
+(s SERIAL PRIMARY KEY,
+x INT,
+y INT);
+""")
+
+#inserting data
+#execute method
+def execute_query(query):
+    cur.execute(query)
+    results = cur.fetchall()
+    print(results)
+
+execute_query("""INSERT INTO demo VALUES
+('g',3,9);""")
+
+
+execute_query("""INSERT INTO demo VALUES
+('v',5,7),('f',8,7);""")
+
+
+execute_query("""SELECT * FROM demo;""")
+
+
+
+
+
+#commit it
+conn.commit()
+
+#close the cursor
+cur.close()
